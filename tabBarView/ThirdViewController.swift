1839e9a482c46d3d116ba40c06abf5602ff0456f
felipesantos22 <felipevs2018@gmail.com>
2025-03-08 09:00:50 -0300

    change tabbar

--- tabBarView/ThirdViewController.swift
+++ tabBarView/ThirdViewController.swift
@@ -11,45 +11,8 @@ class ThirdViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Define a cor de fundo da tela
-        view.backgroundColor = .magenta
-        setupNavigationBar()
-    }
-
-    func setupNavigationBar() {
-        // Customizando a barra de navegação
-        if let navigationController = self.navigationController {
-            // Alterando cor de fundo da navbar
-            navigationController.navigationBar.tintColor = .systemYellow
-            // Tornando a navbar transparente
-            navigationController.navigationBar.isTranslucent = false
-            // Alterando cor de fundo da navbar
-            navigationController.navigationBar.backgroundColor = .white
-        }
-        
-        // Criando um UILabel personalizado para o título
-        let titleLabel = UILabel()
-        titleLabel.text = "Contato"
-        titleLabel.textColor = .black
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        self.navigationItem.titleView = titleLabel
-        
-        // Adicionando um botão de ação à direita da navbar
-        let rightButton = UIBarButtonItem(title: "Ação", style: .plain, target: self, action: #selector(didTapRightButton))
-        self.navigationItem.rightBarButtonItem = rightButton
-        
-        // Adicionando um botão de ação à esquerda da navbar
-        let leftButton = UIBarButtonItem(title: "Voltar", style: .plain, target: self, action: #selector(didTapLeftButton))
-        self.navigationItem.leftBarButtonItem = leftButton
-    }
-    
-    // Ação para o botão da direita
-    @objc func didTapRightButton() {
-        print("Botão da direita pressionado")
-    }
-    
-    // Ação para o botão da esquerda
-    @objc func didTapLeftButton() {
-        print("Botão da esquerda pressionado")
+        view.backgroundColor = .white
+        self.title = "Third"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
     }
 }
